a714c46df07b21a886cfe55a472fcbf2d6fd5a21
ptol1 <ptol1@DESKTOP-017OKHN>
2019-04-08 21:22:25 -0300

    Atualização 08/04 - 1

--- model/dao/AutoresDAO.php
+++ model/dao/AutoresDAO.php
@@ -1,27 +1,14 @@
 <?php
 
-class AutoresDAO implements iDao{
-    private $conexao;
-    private $queryInserir;
-    private $queryListar;
-    private $queryAtualizar;
-    private $queryExcluir;
-    
-    public function __construct() {
-        try {
-            $this->conexao = Conexao::conectar();
-            $this->queryInserir = "INSERT INTO tb_autores(nome_autor) VALUES(:nome_autor)";
-            $this->queryListar = "SELECT * FROM tb_autores";
-            $this->queryAtualizar = "UPDATE tb_autores SET nome_autor = :nome_autor, WHERE id_autores = :id_autores";
-            $this->queryExcluir = "DELETE FROM tb_autores WHERE id_autores = :id_autores";
-        } catch (Exception $exc) {
-            Erro::trataErro($exc);            
-        }
-    }
+require_once '../config/Global.php';
 
-    public function atualizar(Autores $autores) {
+class AutoresDAO implements iDao{
+    
+    public static function atualizar(Autores $autores) {
         try {
-            $stmt = $this->conexao->prepare($this->queryAtualizar);
+            $conexao = Conexao::conectar();
+            $queryAtualizar = "UPDATE tb_autores SET nome_autor = :nome_autor, WHERE id_autores = :id_autores";
+            $stmt = $conexao->prepare($queryAtualizar);
             $stmt->bindValue(':nome_autor', $autores->getNome_autor());
             $stmt->bindValue(':id_autores', $autores->getId_autores());
             $stmt->execute();
@@ -30,18 +17,22 @@ class AutoresDAO implements iDao{
         }
     }
 
-    public function excluir($id) {
+    public static function excluir($id) {
         try {
-            $stmt = $this->conexao->prepare($this->queryExcluir);
+            $conexao = Conexao::conectar();
+            $queryExcluir = "DELETE FROM tb_autores WHERE id_autores = :id_autores";
+            $stmt = $conexao->prepare($queryExcluir);
             $stmt->bindValue(':id_autores', $id);
         } catch (Exception $exc) {
             Erro::trataErro($exc);
         }
     }
 
-    public function inserir(Autores $autores) {
+    public static function inserir(Autores $autores) {
         try {
-            $stmt = $this->conexao->prepare($this->queryInserir);
+            $conexao = Conexao::conectar();
+            $queryInserir = "INSERT INTO tb_autores(nome_autor) VALUES(:nome_autor)";
+            $stmt = $conexao->prepare($queryInserir);
             $stmt->bindValue(':nome_autor', $autores->getNome_autor());
             $stmt->execute();
         } catch (Exception $exc) {
@@ -49,14 +40,20 @@ class AutoresDAO implements iDao{
         }
     }
 
-    public function listar() {
+    public static function listar() {
         try {
-            $stmt = $this->conexao->query($this->queryListar);
+            $conexao = Conexao::conectar();
+            $queryListar = "SELECT * FROM tb_autores";
+            $stmt = $conexao->query($queryListar);
             $stmt->execute();
             return $stmt->fetchAll();
         } catch (Exception $exc) {
             Erro::trataErro($exc);
         }
+    }
+
+    public static function BuscarPorId($id) {
+        
     }
 
 }
--- model/dao/EditoraDAO.php
+++ model/dao/EditoraDAO.php
@@ -1,19 +1,10 @@
 <?php
 
 class EditoraDAO implements iDao{
-    private $conexao;
-    private $queryInserir;
-    private $queryListar;
-    private $queryAtualizar;
-    private $queryExcluir;
     
     public function __construct() {
         try {
-            $this->conexao = Conexao::conectar();
-            $this->queryInserir = "INSERT INTO tb_editora(nome_editora) VALUES(:nome_editora)";
-            $this->queryListar = "SELECT * FROM tb_editora";
-            $this->queryAtualizar = "UPDATE tb_editora SET nome_editora = :nome_editora, WHERE id_editora = :id_editora";
-            $this->queryExcluir = "DELETE FROM tb_editora WHERE id_editora = :id_editora";
+            
         } catch (Exception $exc) {
             Erro::trataErro($exc);            
         }
@@ -21,7 +12,9 @@ class EditoraDAO implements iDao{
 
     public function atualizar(Editora $editora) {
         try {
-            $stmt = $this->conexao->prepare($this->queryAtualizar);
+            $conexao = Conexao::conectar();
+            $queryAtualizar = "UPDATE tb_editora SET nome_editora = :nome_editora, WHERE id_editora = :id_editora";
+            $stmt = $conexao->prepare($this->queryAtualizar);
             $stmt->bindValue(':nome_editora', $editora->getNome_editora());
             $stmt->bindValue(':id_editora', $editora->getId_editora());
             $stmt->execute();
@@ -32,7 +25,9 @@ class EditoraDAO implements iDao{
 
     public function excluir($id) {
         try {
-            $stmt = $this->conexao->prepare($this->queryExcluir);
+            $conexao = Conexao::conectar();
+            $queryExcluir = "DELETE FROM tb_editora WHERE id_editora = :id_editora";
+            $stmt = $conexao->prepare($this->queryExcluir);
             $stmt->bindValue(':id_editora', $id);
         } catch (Exception $exc) {
             Erro::trataErro($exc);
@@ -41,7 +36,9 @@ class EditoraDAO implements iDao{
 
     public function inserir(Editora $editora) {
         try {
-            $stmt = $this->conexao->prepare($this->queryInserir);
+            $conexao = Conexao::conectar();
+            $queryInserir = "INSERT INTO tb_editora(nome_editora) VALUES(:nome_editora)";
+            $stmt = $conexao->prepare($this->queryInserir);
             $stmt->bindValue(':nome_editora', $editora->getNome_editora());
             $stmt->execute();
         } catch (Exception $exc) {
@@ -51,12 +48,18 @@ class EditoraDAO implements iDao{
 
     public function listar() {
         try {
-            $stmt = $this->conexao->query($this->queryListar);
+            $conexao = Conexao::conectar();
+            $queryListar = "SELECT * FROM tb_editora";
+            $stmt = $conexao->query($this->queryListar);
             $stmt->execute();
             return $stmt->fetchAll();
         } catch (Exception $exc) {
             Erro::trataErro($exc);
         }
+    }
+
+    public static function BuscarPorId($id) {
+        
     }
 
 }
